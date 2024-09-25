eb0d310072d7d5ed90ffd3a598e5976ee1a50d1a
Dawid Ratajczak <dawid.ratajczak06@gmail.com>
2024-09-25 12:09:42 +0200

    created project

---
+++ src/main/java/pl/creazy/creazylibbook/CreazyLibBook.java
@@ -0,0 +1,17 @@
+package pl.creazy.creazylibbook;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CreazyLibBook extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
