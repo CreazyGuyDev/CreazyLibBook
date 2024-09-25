cfe262d24cdaaa53f4056540bfe14396fda2a4fd
Dawid Ratajczak <dawid.ratajczak06@gmail.com>
2024-09-25 12:22:57 +0200

    added on disable and enable methods to plugin

--- src/main/java/pl/creazy/creazylibbook/CreazyLibBook.java
+++ src/main/java/pl/creazy/creazylibbook/CreazyLibBook.java
@@ -1,8 +1,21 @@
 package pl.creazy.creazylibbook;
 
+import pl.creazy.creazylib.log.Logger;
+import pl.creazy.creazylib.part.constraints.OnDisable;
+import pl.creazy.creazylib.part.constraints.OnEnable;
+import pl.creazy.creazylib.part.constraints.Part;
 import pl.creazy.creazylib.plugin.CreazyPlugin;
 import pl.creazy.creazylib.plugin.constraints.Plugin;
 
 @Plugin
 public final class CreazyLibBook extends CreazyPlugin {
+  @OnEnable
+  void enable(@Part Logger logger) {
+    logger.info("Loading plugin...");
+  }
+
+  @OnDisable
+  void disable(@Part Logger logger) {
+    logger.info("Disabling plugin...");
+  }
 }
